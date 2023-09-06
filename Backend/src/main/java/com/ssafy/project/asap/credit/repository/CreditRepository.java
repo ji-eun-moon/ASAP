ff971bd5dea1bcf656072695d59a17c5ceb1e5ca
이도하 <victoryddh5@naver.com>
2023-09-06 17:19:21 +0900

    [BE] FIX : 파일명 변경

--- Backend/src/main/java/com/ssafy/project/asap/credit/repository/CreditRepository.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.project.asap.credit.repository;
-
-public interface CreditRepository {
-}
--- Backend/src/main/java/com/ssafy/project/asap/manage/entity/dto/request/CreateManageRequest.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.project.asap.manage.entity.dto.request;
-
-public class CreateManageRequest {
-}

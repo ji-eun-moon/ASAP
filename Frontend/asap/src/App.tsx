076798611f498f0be590d3072727f9381c7fb7a3
문지은 <mjieun0956@gmail.com>
2023-09-11 15:47:46 +0900

    [FE] FEAT : 주요 페이지 파일 생성 및 라우트 연결
    - 페이지 파일 생성 및 라우트 연결
    - material tailwind 설치

--- Frontend/asap/src/App.tsx
+++ Frontend/asap/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import routes from 'router/routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            );
+          })}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ Frontend/asap/src/pages/mypage/AccountPay.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AccountPay() {
+  return <div>AccountPay</div>;
+}
+
+export default AccountPay;

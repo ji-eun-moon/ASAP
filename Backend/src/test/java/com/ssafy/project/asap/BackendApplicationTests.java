43cabaf61393e787f47b993389bf32a09e846cfa
chanwoong114 <cksdnd10001@gmail.com>
2023-09-12 17:40:39 +0900

    [BE] FEAT: APIServer 프로젝트 생성

--- Backend/src/test/java/com/ssafy/project/asap/BackendApplicationTests.java
+++ Backend/src/test/java/com/ssafy/project/asap/BackendApplicationTests.java
@@ -1,13 +1,7 @@
 package com.ssafy.project.asap;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }

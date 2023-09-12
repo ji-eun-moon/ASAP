07f0240e4417223794bce174c964ff52fea080f3
문지은 <mjieun0956@gmail.com>
2023-09-12 17:24:27 +0900

    [FE] FEAT : 회원가입 테스트코드 작성

---
+++ Frontend/asap/src/hooks/api/auth/useSignUp.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+/**
+ * 회원 가입
+ */
+
+interface UserInfo {
+  id: string;
+  password: string;
+  name: string;
+  email: string;
+}
+
+const useSignUp = () => {
+  const signUp = async ({ id, password, name, email }: UserInfo) => {
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: 'http://localhost:8080/api/v1/member/signup',
+        data: { id, password, name, email },
+      });
+      // 서버에서 받은 응답 처리
+      if (response.status === 200) {
+        console.log('회원가입 성공:', response.data);
+      } else {
+        console.error('회원가입 실패:', response.data);
+      }
+    } catch (error) {
+      console.log('서버 오류:', error);
+    }
+  };
+
+  return { signUp };
+};
+
+export default useSignUp;

f30a5d3aafa4fd053a1bb368d4a92842189ef717
문지은 <mjieun0956@gmail.com>
2023-09-13 17:47:26 +0900

    [FE] TEST : 테스트 코드 axios 주소 변경

--- Frontend/asap/src/hooks/api/auth/useSignUp.ts
+++ Frontend/asap/src/hooks/api/auth/useSignUp.ts
@@ -16,11 +16,11 @@ const useSignUp = () => {
     try {
       const response = await axios({
         method: 'POST',
-        url: 'http://j9c202.p.ssafy.io:9000/api/v1/member/signup',
+        url: 'https://j9c202.p.ssafy.io/api/v1/member/signup',
         data: { id, password, name, email },
       });
       // 서버에서 받은 응답 처리
-      if (response.status === 200) {
+      if (response.status === 201) {
         console.log('회원가입 성공:', response.data);
       } else {
         console.error('회원가입 실패:', response.data);

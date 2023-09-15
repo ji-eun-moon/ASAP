1989eb0203f805c011b0957cf9e337a66eb759bc
박서희 <dudntjgml@gmail.com>
2023-09-15 14:15:23 +0900

    [FE] TEST : 관리자 테스트코드 작성

--- Frontend/asap/src/hooks/api/admin/useAdminApiProgress.ts
+++ Frontend/asap/src/hooks/api/admin/useAdminApiProgress.ts
@@ -1,23 +1,17 @@
-import axios from 'axios';
+import axiosInstance from 'utils/axiosInstance';
 
 interface postInfo {
-  idx: number;
-  status: string;
+  applyId: number;
+  progress: string;
 }
 const useAdminApiProgress = () => {
-  const adminApiProgress = async ({ idx, status }: postInfo) => {
+  const adminApiProgress = async ({ applyId, progress }: postInfo) => {
+    console.log(applyId, progress);
     try {
-      const response = await axios({
-        method: 'POST',
-        url: 'http://j9c202.p.ssafy.io:9000/api/v1/apply/progress',
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzUxMiJ9.eyJpZCI6InN0cmluZyIsImlhdCI6MTY5NDU4NjUyMSwiZXhwIjoxNjk0NTg2NjA3fQ.vTpyRNJm0Ydb548t9F0P4B_7Gi24TQDS3MTMG4CR7QH2FYI0tCw4cfbFHMbAahI6mXaqmXJOXO8DOUZPar2xQQ',
-        },
-        data: {
-          idx,
-          status,
-        },
+      const response = await axiosInstance({
+        method: 'PUT',
+        url: '/api/v1/apply/progress',
+        data: { applyId, progress },
       });
       if (response.status === 200) {
         console.log('진행 상태 변경 완료');

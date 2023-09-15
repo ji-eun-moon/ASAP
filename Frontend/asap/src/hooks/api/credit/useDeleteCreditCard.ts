aa2fdc79d2ef854336fd0c383146a289c9650e4f
sion <sion941004@naver.com>
2023-09-15 16:42:56 +0900

    [FE] FEAT : 카드 등록하기 구현

--- Frontend/asap/src/hooks/api/credit/useDeleteCreditCard.ts
+++ Frontend/asap/src/hooks/api/credit/useDeleteCreditCard.ts
@@ -1,18 +1,18 @@
 import axiosInstance from 'utils/axiosInstance';
 
 const useDeleteCredit = () => {
-  const deleteCredit = async () => {
+  const deleteCreditCard = async () => {
     try {
       const response = await axiosInstance({
         method: 'DELETE',
         url: `/api/v1/credit`,
       });
-      console.log('성공', response.data);
+      console.log('삭제성공', response.data);
     } catch (error) {
       console.log('서버 오류:', error);
     }
   };
-  return { deleteCredit };
+  return { deleteCreditCard };
 };
 
 export default useDeleteCredit;
--- Frontend/asap/src/pages/mypage/AccountPay.tsx
+++ Frontend/asap/src/pages/mypage/AccountPay.tsx
@@ -1,19 +1,81 @@
-import useDeleteCredit from 'hooks/api/credit/useDeleteCredit';
-import React from 'react';
+import useDeleteCredit from 'hooks/api/credit/useDeleteCreditCard';
+import useGetCreditCard from 'hooks/api/credit/useGetCreditCard';
+import usePostCreditCard from 'hooks/api/credit/usePostCreditCard';
+import React, { useState, ChangeEvent } from 'react';
+import { Input, Button } from '@material-tailwind/react';
 
 function AccountPay() {
-  const { deleteCredit } = useDeleteCredit();
+  const { deleteCreditCard } = useDeleteCredit();
+  const { getCreditCard } = useGetCreditCard();
+  const { postCreditCard } = usePostCreditCard();
+
+  // 카드등록 정보 (카드회사, 카드번호)
+  const [cardCompany, setCardCompany] = useState<string>(''); // 카드회사
+  const [cardNumber, setCardNumber] = useState<string>(''); // 카드번호
+
+  const onCompanyHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setCardCompany(event.target.value);
+  };
+
+  const onNumberHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setCardNumber(event.target.value);
+  };
+
+  const onPostCreditCard = async () => {
+    await postCreditCard({
+      cardCompany,
+      cardNumber,
+    });
+  };
 
   return (
     <div>
       AccountPay
-      <button
-        type="button"
-        style={{ border: '1px solid' }}
-        onClick={deleteCredit}
-      >
-        카드 삭제하기 테스트
-      </button>
+      <div>
+        <button
+          type="button"
+          style={{ border: '1px solid' }}
+          onClick={getCreditCard}
+        >
+          카드 조회하기 테스트
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          style={{ border: '1px solid' }}
+          onClick={deleteCreditCard}
+        >
+          카드 삭제하기 테스트
+        </button>
+      </div>
+      <div>
+        카드 등록하기 테스트
+        <div>
+          <p>카드회사</p>
+          <Input
+            label="카드회사"
+            value={cardCompany}
+            onChange={onCompanyHandler}
+            className="pr-20"
+            containerProps={{
+              className: 'min-w-0',
+            }}
+            crossOrigin=""
+          />
+          <Input
+            label="카드번호"
+            value={cardNumber}
+            onChange={onNumberHandler}
+            className="pr-20"
+            containerProps={{
+              className: 'min-w-0',
+            }}
+            crossOrigin=""
+          />
+        </div>
+        <Button onClick={onPostCreditCard}>카드등록</Button>
+      </div>
     </div>
   );
 }

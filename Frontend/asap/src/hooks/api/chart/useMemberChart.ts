9af069d2ee46f7317816ebaf792ebe2067802a7a
박서희 <dudntjgml@gmail.com>
2023-09-15 14:13:44 +0900

    [FE] TEST : 사용자 및 제공자 테스트코드 작성(axios 주소 변경)

--- Frontend/asap/src/hooks/api/chart/useMemberChart.ts
+++ Frontend/asap/src/hooks/api/chart/useMemberChart.ts
@@ -1,15 +1,11 @@
-import axios from 'axios';
+import axiosInstance from 'utils/axiosInstance';
 
 const useMemberChart = () => {
   const memberChart = async () => {
     try {
-      const response = await axios({
+      const response = await axiosInstance({
         method: 'GET',
-        url: 'http://j9c202.p.ssafy.io:9000/api/v1/member/use-api',
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzUxMiJ9.eyJpZCI6InN0cmluZyIsImlhdCI6MTY5NDU4ODQ2MCwiZXhwIjoxNjk0NTg4NTQ3fQ.hdqZEIIL8u6B-QEcBuncpFvX_yFxWnXCl7wAXm1NYD4Ohx0U3CkzPOkHAtjUlX9BDWlaot1evYHHcDnQCgLSVA',
-        },
+        url: '/api/v1/monthly/usage', // 아직 안만들어짐
       });
       if (response.status === 200) {
         console.log('사용자 통계 정보', response.data);

6112c5379acddba53c826acdcc352a4d10c23440
송아람 <0807com@naver.com>
2023-09-15 09:42:23 +0900

    [FE] TEST : api 조회 테스트코드 인스턴스 수정

--- Frontend/asap/src/hooks/api/api/useGetSupplyList.ts
+++ Frontend/asap/src/hooks/api/api/useGetSupplyList.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axiosInstance from 'utils/axiosInstance';
 import { useState, useEffect } from 'react';
 
 interface IApi {
@@ -10,13 +10,9 @@ const useGetSupplyList = () => {
   const [supplyList, setSupplyList] = useState<IApi[] | null>(null);
   const getSupplyList = async () => {
     try {
-      const response = await axios({
+      const response = await axiosInstance({
         method: 'GET',
         url: 'https://j9c202.p.ssafy.io/api/v1/member/provide-api', // api 수정 필요
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzUxMiJ9.eyJpZCI6InNvbmciLCJpYXQiOjE2OTQ2NTQ4NjcsImV4cCI6MTY5NDY1NDk1NH0.tJH1m3yEVxc8GZ_9DYCbA1uStI11c44YKP-DvsTZfFLYXH33nyYQGZef__9X_CoRAGoamakRNsbtzUKH3jtMuQ',
-        },
       });
       setSupplyList(response.data);
     } catch (error) {

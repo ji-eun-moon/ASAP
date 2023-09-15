a22c057dec9853c7db06deca381b0aef8c22e404
문지은 <mjieun0956@gmail.com>
2023-09-15 11:48:29 +0900

    [FE] TEST : 회원 정보 찾기, 회원 정보 수정 테스트 코드 작성

---
+++ Frontend/asap/src/hooks/api/mypage/useModifyAccount.ts
@@ -0,0 +1,24 @@
+import axiosInstance from 'utils/axiosInstance';
+
+interface IModifyMember {
+  name: string;
+  email: string;
+}
+const useModifyAccount = () => {
+  const modifyMemberInfo = async ({ name, email }: IModifyMember) => {
+    try {
+      const response = await axiosInstance({
+        method: 'POST',
+        url: '/api/v1/member/me/',
+        data: { name, email },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log('서버 오류:', error);
+    }
+  };
+
+  return { modifyMemberInfo };
+};
+
+export default useModifyAccount;

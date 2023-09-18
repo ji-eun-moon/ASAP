1c8bce6797e4ad262347d91a4040db6b763c1ee7
victoryddh <victoryddh5@naver.com>
2023-09-18 22:50:20 +0900

    [BE] FIX : Member 관련 코드 수정

--- Backend/src/main/java/com/ssafy/project/asap/member/service/MemberService.java
+++ Backend/src/main/java/com/ssafy/project/asap/member/service/MemberService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.webjars.NotFoundException;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;

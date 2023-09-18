70aeebd2dba8a7cb7d053be7ad440eb2e5048f36
문지은 <mjieun0956@gmail.com>
2023-09-18 16:58:01 +0900

    [FE] FEAT : 회원 가입 페이지 작성
    - 타이머 컴포넌트 구현
    - 이메일 인증코드 확인 결과별 알림 추가 필요

--- Frontend/asap/src/pages/auth/SignUp.tsx
+++ Frontend/asap/src/pages/auth/SignUp.tsx
@@ -1,87 +1,166 @@
 import React, { useState, ChangeEvent } from 'react';
+import { Link } from 'react-router-dom';
+import 'styles/auth/SignUp.scss';
 import useSignUp from 'hooks/api/auth/useSignUp';
 import useCheckId from 'hooks/api/auth/useCheckId';
 import { Input, Button } from '@material-tailwind/react';
 import useAuthEmail from 'hooks/api/auth/useAuthEmail';
+import Timer from 'components/auth/Timer';
+
+interface ITime {
+  mm: number;
+  ss: number;
+}
 
 function SignUp() {
   const { signUp } = useSignUp();
   const { isIdAvailable, setIsIdAvailable, checkIdAvailability } = useCheckId();
-  const { postEmail, checkEmailCode } = useAuthEmail();
+  const {
+    postEmail,
+    checkEmailCode,
+    postedEmail,
+    checkedCode,
+    resetEmailStatus,
+  } = useAuthEmail();
 
   const [userId, setUserId] = useState<string>('');
 
   const [userPassword, setUserPassword] = useState<string>('');
   const [userConfirmPassword, setUserConfirmPassword] = useState<string>('');
 
+  // 비밀 번호 조건
+  const hasUpperCase = /[A-Z]/.test(userPassword);
+  const hasLowerCase = /[a-z]/.test(userPassword);
+  const hasNumber = /[0-9]/.test(userPassword);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(userPassword);
+  const isLengthValid = userPassword.length >= 8 && userPassword.length <= 16;
+
   const [userName, setUserName] = useState<string>('');
 
   const [userEmail, setUserEmail] = useState<string>('');
   const [emailCode, setEmailCode] = useState<string>('');
+  const [initialTime, setInitialTime] = useState<ITime>({ mm: 5, ss: 0 });
+  const [timerKey, setTimerKey] = useState<number>(0);
 
+  // 아이디 입력
   const onIdHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setIsIdAvailable(false);
     setUserId(event.target.value);
   };
 
+  // 아이디 중복 확인 버튼 클릭
   const onIdCheckHandler = async () => {
     await checkIdAvailability(userId);
   };
 
+  // 비밀 번호 입력
   const onPasswordHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setUserPassword(event.target.value);
   };
 
+  // 비밀 번호 확인 입력
   const onConfirmPasswordHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setUserConfirmPassword(event.target.value);
   };
 
-  // 비밀 번호 조건 확인
+  // 비밀 번호 조건 확인 메시지
   const passwordMessage = () => {
-    if (!userPassword) {
-      return <p>비밀번호는 필수 입력값입니다.</p>;
+    if (
+      !hasUpperCase ||
+      !hasLowerCase ||
+      !hasNumber ||
+      !hasSpecialChar ||
+      !isLengthValid
+    ) {
+      return (
+        <p className="my-2 ms-1 text-red-600">
+          비밀번호는 대소문자/숫자/특수문자 혼용 8자이상 16자 이하로
+          입력해주세요.
+        </p>
+      );
     }
-    if (userPassword && userPassword === userConfirmPassword) {
-      return <p>사용 가능한 비밀번호 입니다.</p>;
-    }
-    return <p>비밀번호를 확인해주세요.</p>;
+
+    return <p className="my-2 ms-1 text-blue">사용 가능한 비밀번호입니다.</p>;
   };
 
+  // 비밀번호 확인 체크 메시지
+  const passwordCheckMessage = () => {
+    if (!userPassword || !userConfirmPassword) {
+      return null;
+    }
+    if (userPassword === userConfirmPassword) {
+      return <p className="my-2 ms-1 text-blue">비밀번호가 일치합니다.</p>;
+    }
+    return (
+      <p className="my-2 ms-1 text-red-600">비밀번호가 일치하지 않습니다.</p>
+    );
+  };
+
+  // 이름 입력
   const onNameHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setUserName(event.target.value);
   };
 
-  const onEmailHandler = (event: ChangeEvent<HTMLInputElement>) =>
+  // 이메일 입력
+  const onEmailHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setUserEmail(event.target.value);
+  };
 
+  // 이메일 인증 코드 전송
   const onPostEmailCode = async () => {
+    resetEmailStatus();
     if (!userEmail) {
       alert('이메일을 입력해주세요');
       return;
     }
     await postEmail(userEmail);
+    setInitialTime({ mm: 5, ss: 0 });
+    setTimerKey((prevKey) => prevKey + 1);
   };
 
+  // 인증 코드 입력
   const onEmailCodeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     setEmailCode(event?.target.value);
   };
 
+  // 인증 코드 확인
   const onCheckEmailCode = async () => {
     await checkEmailCode({ email: userEmail, code: emailCode });
   };
 
-  const onsubmitHandler = async () => {
-    // 회원가입 제출 조건 추가 필요
+  // 타이머 출력
+  const codeTimer = () => {
+    if (postedEmail && !checkedCode) {
+      return <Timer key={timerKey} mm={initialTime.mm} ss={initialTime.ss} />;
+    }
+    return null;
+  };
 
-    // 아이디 중복 확인 후에 회원가입 진행
+  const onsubmitHandler = async () => {
     if (!isIdAvailable) {
-      alert('아이디 중복 확인이 필요합니다.');
+      alert('아이디를 다시 확인해주세요.');
       return;
     }
 
-    // 비밀번호와 비밀번호 확인 같지 않으면 회원가입 불가능
-    if (userPassword !== userConfirmPassword) {
-      alert('비밀번호를 다시 확인해주세요');
+    if (
+      userPassword !== userConfirmPassword ||
+      !hasUpperCase ||
+      !hasLowerCase ||
+      !hasNumber ||
+      !hasSpecialChar ||
+      !isLengthValid
+    ) {
+      alert('비밀번호를 다시 확인해주세요.');
+      return;
+    }
+
+    if (!userName) {
+      alert('이름을 입력해주세요.');
+      return;
+    }
+
+    if (!checkedCode) {
+      alert('이메일 인증을 확인해주세요.');
       return;
     }
 
@@ -92,84 +171,91 @@ function SignUp() {
       email: userEmail,
     });
   };
+
   return (
-    <div className="container mx-auto">
-      <p className="font-bold">Sign Up</p>
+    <div className="container mx-auto form-container page-container">
+      <div className="flex justify-center my-8">
+        <p className="font-bold text-6xl text-blue">Sign Up</p>
+      </div>
 
       {/* 아이디 입력 */}
-      <p>아이디</p>
-      <div className="relative flex w-full my-3">
-        <Input
-          label="아이디"
-          value={userId}
-          onChange={onIdHandler}
-          className="pr-20"
-          containerProps={{
-            className: 'min-w-0',
-          }}
-          crossOrigin=""
-        />
-        {isIdAvailable ? (
-          <Button
-            size="sm"
-            color="blue"
-            className="!absolute right-1 top-1 rounded"
-            ripple={false}
-            style={{ cursor: 'auto' }}
-          >
-            아이디 사용 가능
-          </Button>
-        ) : (
-          <Button
-            size="sm"
-            color={userId ? 'gray' : 'blue-gray'}
-            disabled={!userId}
-            className="!absolute right-1 top-1 rounded"
-            onClick={onIdCheckHandler}
-          >
-            아이디 중복확인
-          </Button>
-        )}
+      <div className="mb-7">
+        <p className="text-lg font-extrabold text-blue">아이디</p>
+        <div className="relative flex w-full my-3">
+          <Input
+            label="아이디"
+            value={userId}
+            onChange={onIdHandler}
+            className="pr-20"
+            containerProps={{
+              className: 'min-w-0',
+            }}
+            crossOrigin=""
+          />
+          {isIdAvailable ? (
+            <Button
+              size="sm"
+              color="blue"
+              className="!absolute right-1 top-1 rounded btn-width"
+              ripple={false}
+              style={{ cursor: 'auto' }}
+            >
+              아이디 중복 확인
+            </Button>
+          ) : (
+            <Button
+              size="sm"
+              color={userId ? 'gray' : 'blue-gray'}
+              disabled={!userId}
+              className="!absolute right-1 top-1 rounded btn-width"
+              onClick={onIdCheckHandler}
+            >
+              아이디 중복 확인
+            </Button>
+          )}
+        </div>
       </div>
 
       {/* 비밀번호 입력 */}
-      <p>비밀번호</p>
-      <div className="mt-3">
-        <Input
-          type="password"
-          value={userPassword}
-          onChange={onPasswordHandler}
-          label="비밀번호"
-          crossOrigin=""
-        />
-      </div>
-      <div className="my-3">
-        <Input
-          type="password"
-          value={userConfirmPassword}
-          onChange={onConfirmPasswordHandler}
-          label="비밀번호 확인"
-          crossOrigin=""
-        />
-        <p>
-          비밀번호는 영문자/숫자/특수문자 혼용 8자이상 16자 이하로 입력해주세요.
-        </p>
-        {passwordMessage()}
+      <div className="mb-7">
+        <p className="text-lg font-extrabold text-blue">비밀번호</p>
+        <div className="mt-3">
+          <Input
+            type="password"
+            value={userPassword}
+            onChange={onPasswordHandler}
+            label="비밀번호"
+            crossOrigin=""
+          />
+        </div>
+        <div className="my-3">
+          <Input
+            type="password"
+            value={userConfirmPassword}
+            onChange={onConfirmPasswordHandler}
+            label="비밀번호 확인"
+            crossOrigin=""
+          />
+          {passwordMessage()}
+          {passwordCheckMessage()}
+        </div>
       </div>
 
       {/* 이름 입력 */}
-      <p>이름</p>
-      <div className="my-3">
-        <Input
-          label="이름"
-          value={userName}
-          onChange={onNameHandler}
-          crossOrigin=""
-        />
+      <div className="mb-7">
+        <p className="text-lg font-extrabold text-blue">이름</p>
+        <div className="my-3">
+          <Input
+            label="이름"
+            value={userName}
+            onChange={onNameHandler}
+            crossOrigin=""
+          />
+        </div>
       </div>
 
       {/* 이메일 입력 */}
-      <p>이메일</p>
+      <p className="text-lg font-extrabold text-blue">이메일</p>
       <div className="relative flex w-full my-3">
         <Input
           type="email"
@@ -182,15 +268,27 @@ function SignUp() {
           }}
           crossOrigin=""
         />
-        <Button
-          size="sm"
-          color={userEmail ? 'gray' : 'blue-gray'}
-          disabled={!userEmail}
-          className="!absolute right-1 top-1 rounded"
-          onClick={onPostEmailCode}
-        >
-          인증 코드 전송
-        </Button>
+        {postedEmail ? (
+          <Button
+            size="sm"
+            color="blue"
+            disabled={!userEmail}
+            className="!absolute right-1 top-1 rounded btn-width"
+            onClick={onPostEmailCode}
+          >
+            인증 코드 재전송
+          </Button>
+        ) : (
+          <Button
+            size="sm"
+            color={userEmail ? 'gray' : 'blue-gray'}
+            disabled={!userEmail}
+            className="!absolute right-1 top-1 rounded btn-width"
+            onClick={onPostEmailCode}
+          >
+            인증 코드 전송
+          </Button>
+        )}
       </div>
       <div className="relative flex w-full my-3">
         <Input
@@ -204,21 +302,47 @@ function SignUp() {
           }}
           crossOrigin=""
         />
-        <Button
-          size="sm"
-          color={emailCode ? 'gray' : 'blue-gray'}
-          disabled={!emailCode}
-          className="!absolute right-1 top-1 rounded"
-          onClick={onCheckEmailCode}
-        >
-          인증 코드 확인
-        </Button>
+        <div className="flex !absolute right-1 top-1">
+          {codeTimer()}
+          {checkedCode ? (
+            <Button
+              size="sm"
+              color="blue"
+              disabled={!emailCode}
+              className="rounded btn-width"
+              onClick={onCheckEmailCode}
+            >
+              인증 완료
+            </Button>
+          ) : (
+            <Button
+              size="sm"
+              color={emailCode ? 'gray' : 'blue-gray'}
+              disabled={!emailCode}
+              className="rounded btn-width"
+              onClick={onCheckEmailCode}
+            >
+              인증 코드 확인
+            </Button>
+          )}
+        </div>
       </div>
       {/* 회원가입 버튼 */}
-      <div className="flex flex-row-reverse">
-        <Button ripple onClick={onsubmitHandler}>
+      <div className="flex justify-center my-9">
+        <Button ripple onClick={onsubmitHandler} className="bg-blue btn-width">
           회원 가입
         </Button>
+      </div>
+
+      <hr className="line-blue" />
+
+      <div className="flex justify-center my-5">
+        <p className="text-xl text-gray-500 font-bold me-3">
+          이미 회원이신가요 ?
+        </p>
+        <Link to="/login" className="text-xl font-bold text-blue">
+          Login
+        </Link>
       </div>
     </div>
   );

3f62dc37c25a5354e4bea4dfa51561da27317e05
victoryddh <victoryddh5@naver.com>
2023-09-19 21:23:21 +0900

    [BE] FIX : Notice 관련 코드 수정

--- Backend/src/main/java/com/ssafy/project/asap/notice/entity/dto/response/FindNoticesResponse.java
+++ Backend/src/main/java/com/ssafy/project/asap/notice/entity/dto/response/FindNoticesResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.project.asap.notice.entity.dto.response;
 
+import com.ssafy.project.asap.notice.entity.domain.Notice;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,4 +16,11 @@ public class FindNoticesResponse {
     private boolean isRead;
     private LocalDateTime createDate;
 
+    public FindNoticesResponse(Notice notice) {
+        this.noticeId = notice.getNoticeId();
+        this.title = notice.getTitle();
+        this.content = notice.getContent();
+        this.isRead = notice.isRead();
+        this.createDate = notice.getCreateDate();
+    }
 }
--- Backend/src/main/java/com/ssafy/project/asap/notice/service/NoticeService.java
+++ Backend/src/main/java/com/ssafy/project/asap/notice/service/NoticeService.java
@@ -2,11 +2,16 @@ package com.ssafy.project.asap.notice.service;
 
 import com.ssafy.project.asap.apply.entity.dto.request.RejectApplyRequest;
 import com.ssafy.project.asap.apply.repository.ApplyRepository;
+import com.ssafy.project.asap.member.entity.domain.Member;
 import com.ssafy.project.asap.notice.entity.domain.Notice;
+import com.ssafy.project.asap.notice.entity.dto.response.FindNoticesResponse;
 import com.ssafy.project.asap.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +21,6 @@ public class NoticeService {
     private final NoticeRepository noticeRepository;
     private final ApplyRepository applyRepository;
 
-    @Transactional
     public void saveRejectApply(RejectApplyRequest request){
 
         Notice notice = Notice.builder()
@@ -28,4 +32,47 @@ public class NoticeService {
         noticeRepository.save(notice);
     }
 
+    @Transactional
+    public void update(Long noticeId){
+
+        noticeRepository.findByNoticeId(noticeId).setRead(true);
+
+    }
+
+    @Transactional
+    public List<FindNoticesResponse> findAll(Member member){
+
+        List<FindNoticesResponse> list = new ArrayList<>();
+
+        for(Notice notice : noticeRepository.findAllByMember(member)){
+            list.add(new FindNoticesResponse(notice));
+        }
+
+        return list;
+    }
+
+    public Long findUnRead(Member member) {
+
+        List<Notice> list = noticeRepository.findAllByMember(member);
+
+        Long unReadMessage = 0L;
+
+        for(Notice notice : list){
+            if(notice.isRead() == false){
+                unReadMessage++;
+            }
+        }
+
+        return unReadMessage;
+
+    }
+
+    @Transactional
+    public void delete(Long noticeId) {
+
+        Notice notice = noticeRepository.findByNoticeId(noticeId);
+
+        noticeRepository.delete(notice);
+
+    }
 }

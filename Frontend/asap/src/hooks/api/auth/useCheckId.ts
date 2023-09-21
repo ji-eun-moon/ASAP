b927c7fa327286a7ecdabe5aae1732b571e5fc36
송아람 <0807com@naver.com>
2023-09-21 15:20:34 +0900

    [FE] FIX : 아이디 중복 체크 수정

--- Frontend/asap/src/hooks/api/auth/useCheckId.ts
+++ Frontend/asap/src/hooks/api/auth/useCheckId.ts
@@ -11,15 +11,15 @@ const useCheckId = () => {
         url: 'https://j9c202.p.ssafy.io/api/v1/member/check-id',
         data: { id },
       });
-      // 아이디 체크 조건문 수정 필요
-      if (response.status === 200) {
-        // 중복 아이디가 없는 경우
-        setIsIdAvailable(true);
-        alert('사용가능한 아이디입니다.');
-      } else {
+
+      if (response.data === 'USER_ID_DUPLICATED') {
         // 중복 아이디가 있는 경우
         setIsIdAvailable(false);
         alert('사용할 수 없는 아이디입니다. 다른 아이디를 입력해주세요');
+      } else {
+        // 중복 아이디가 없는 경우
+        setIsIdAvailable(true);
+        alert('사용가능한 아이디입니다.');
       }
     } catch (error) {
       console.error('서버 오류:', error);

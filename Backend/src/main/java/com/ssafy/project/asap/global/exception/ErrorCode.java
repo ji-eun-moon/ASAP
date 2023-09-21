398846517a2628b2c00d7dba3e0ff8f3da4fb3d7
victoryddh <victoryddh5@naver.com>
2023-09-21 23:21:05 +0900

    [BE] FEAT : ErrorCode 작성

--- Backend/src/main/java/com/ssafy/project/asap/global/exception/ErrorCode.java
+++ Backend/src/main/java/com/ssafy/project/asap/global/exception/ErrorCode.java
@@ -17,7 +17,8 @@ public enum ErrorCode{
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 회원이 존재하지 않습니다."),
     SIGNUP_DUPLICATED(HttpStatus.CONFLICT, "회원가잆 오류입니다."),
     EMAIL_CODE_NOT_AUTHORIZED(HttpStatus.NON_AUTHORITATIVE_INFORMATION, "올바른 인증번호가 아닙니다."),
-    EMAIL_NOT_SEND(HttpStatus.BAD_REQUEST, "인증코드 전송에 실패했습니다.");
+    EMAIL_NOT_SEND(HttpStatus.BAD_REQUEST, "인증코드 전송에 실패했습니다."),
+    PURPOSE_DUPLICATED(HttpStatus.CONFLICT, "이미 등록된 API입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;

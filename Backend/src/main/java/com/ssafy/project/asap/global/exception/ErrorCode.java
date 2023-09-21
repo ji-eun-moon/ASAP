c185d4ae3986d08b0720ff09611e4ccc95db4bc0
이도하 <victoryddh5@naver.com>
2023-09-21 16:19:44 +0900

    [BE] FEAT : ErrorCode 작성

--- Backend/src/main/java/com/ssafy/project/asap/global/exception/ErrorCode.java
+++ Backend/src/main/java/com/ssafy/project/asap/global/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode{
 
     USER_ID_DUPLICATED(HttpStatus.CONFLICT, "이미 등록된 아이디입니다."),
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지않은 카테고리입니다.");
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지않은 카테고리입니다."),
+    USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND, "잘못된 아이디입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;

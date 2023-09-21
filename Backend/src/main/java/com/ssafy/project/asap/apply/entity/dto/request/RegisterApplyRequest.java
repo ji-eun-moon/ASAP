6027e4387ec1274e55c0d648cb056fc4ec8c0410
박서희 <dudntjgml@gmail.com>
2023-09-21 15:15:43 +0900

    [BE] FEAT : APPLY 신청 어노테이션 수정

--- Backend/src/main/java/com/ssafy/project/asap/apply/entity/dto/request/RegisterApplyRequest.java
+++ Backend/src/main/java/com/ssafy/project/asap/apply/entity/dto/request/RegisterApplyRequest.java
@@ -1,13 +1,17 @@
 package com.ssafy.project.asap.apply.entity.dto.request;
 
 import com.ssafy.project.asap.apply.entity.domain.ApplyMethod;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterApplyRequest {
 
     private String title;

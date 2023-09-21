7f7510f592d31af632517147bc3f1d2fdd86d02f
박서희 <dudntjgml@gmail.com>
2023-09-21 21:30:31 +0900

    [FE] FEAT : 관리자 api 상세 페이지 수정

--- Frontend/asap/src/pages/admin/ApiApproval.tsx
+++ Frontend/asap/src/pages/admin/ApiApproval.tsx
@@ -8,7 +8,119 @@ import useAdminApiDetail from 'hooks/api/admin/useAdminApiDetail';
 
 import Header from 'components/common/Header';
 import Modal from 'components/common/Modal';
+import { Tabs, TabsHeader, Tab, Card } from '@material-tailwind/react';
+import { Collapse, Ripple, initTE } from 'tw-elements';
 
+// import useSubmitApi from 'hooks/api/api/useSubmitApi';
+
+// import SubmitTags from 'components/supply/SubmitTags';
+// import useSubmitStore from 'store/supply/useSubmitStore';
+// import SubmitInput from 'components/supply/SubmitInput';
+// import SubmitOutput from 'components/supply/SubmitOutput';
+// import Calendar from 'components/common/Calendar';
+import Table from 'components/mypage/InfoTable';
+import 'styles/common/Input.scss';
+import JsonTable from 'components/common/JsonTable';
+
+initTE({ Collapse, Ripple });
+
+function ApiSupply({ apiDetail = {} }: any) {
+  const TABLE_HEAD = ['key', 'name', 'type', 'required', 'description'];
+  console.log('tags', apiDetail.tags);
+  return (
+    <div className="my-5">
+      {!apiDetail ? (
+        <div>Loading</div>
+      ) : (
+        // {/* 표 테두리 */}
+        <div className="border-2">
+          <Table left="API 제목" right={apiDetail?.title} height="55px" />
+          <hr />
+          <Table left="EndPoint" right={apiDetail?.api} height="55px" />
+          <hr />
+          <Table left="Method" right={apiDetail?.method} height="55px" />
+          <hr />
+          <Table left="상세 내용" right={apiDetail?.content} height="55px" />
+          <hr />
+          <Table left="1회 요청 비용" right={apiDetail?.price} height="55px" />
+          <hr />
+          <Table
+            left="제공 종료 날짜"
+            right={apiDetail?.provideDate}
+            height="55px"
+          />
+          <hr />
+          <Table
+            left="관련 태그"
+            right={
+              apiDetail.tags
+                ? JSON.parse(apiDetail.tags).map((tag: string) => <p>#{tag}</p>)
+                : ''
+            }
+            height="200px"
+          />
+
+          <hr />
+          <Table
+            left="INPUT"
+            right={
+              <Card className="w-full h-full container mx-auto p-5 bg-gray-200">
+                <div className="grid grid-cols-5 bg-gray-200">
+                  {TABLE_HEAD.map((head) => (
+                    <p
+                      key={head}
+                      className="col-span-1 p-2 font-bold text-xl h-11"
+                    >
+                      {head}
+                    </p>
+                  ))}
+                </div>
+                <hr className="h-0.5 bg-gray-500" />
+                <JsonTable jsonData={apiDetail.input} />
+              </Card>
+            }
+            height="200px"
+          />
+
+          <hr />
+          <Table
+            left="OUTPUT"
+            right={
+              <Card className="w-full h-full container mx-auto p-5 bg-gray-200">
+                <div className="grid grid-cols-5 bg-gray-200">
+                  {TABLE_HEAD.map((head) => (
+                    <p
+                      key={head}
+                      className="col-span-1 p-2 font-bold text-xl h-11"
+                    >
+                      {head}
+                    </p>
+                  ))}
+                </div>
+                <hr className="h-0.5 bg-gray-500" />
+                <JsonTable jsonData={apiDetail.output} />
+              </Card>
+            }
+            height="200px"
+          />
+
+          <hr />
+          <Table
+            left="API 제공 신청자"
+            right={`${apiDetail?.name} (${apiDetail?.id})`}
+            height="55px"
+          />
+          <hr />
+          <Table
+            left="제공 신청 날짜"
+            right={apiDetail?.createDate.split('T')[0]}
+            height="55px"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
 function ApiApproval() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -25,12 +137,6 @@ function ApiApproval() {
   const [stateApis, setStateApis] = useState(apis);
   const { adminApiRejectReason } = useAdminApiRejectReason();
   const { adminApiDetail, apiDetail } = useAdminApiDetail();
-
-  const [selectedItem, setSelectItem] = useState('전체 조회');
-  const handleItemClick = (item: string) => {
-    setSelectItem(item);
-    setStateApis(apis.filter((api) => api.progress === item.slice(0, 2)));
-  };
 
   const [nowApiId, setNowApiId] = useState(-1);
   const [nowApiTitle, setNowApiTitle] = useState('');
@@ -91,11 +197,29 @@ function ApiApproval() {
     setNowApiTitle(title);
   };
 
-  // const [detailApplyId, setDetailApplyId] = useState(-1);
+  const [detailApplyId, setDetailApplyId] = useState<number | null>(null);
+  const [isOpened, setIsOpened] = useState(false);
   const showDetail = async (applyId: number) => {
     console.log(applyId);
-    adminApiDetail(applyId);
+    if (detailApplyId === applyId) {
+      setIsOpened(!isOpened);
+      console.log('이전과 같습니다');
+    } else {
+      setIsOpened(true);
+      setDetailApplyId(applyId);
+      adminApiDetail(applyId);
+      setIsModalOpen(!isModalOpen);
+    }
   };
+
+  const [selectedItem, setSelectItem] = useState('전체 조회');
+  const handleItemClick = (item: string) => {
+    setSelectItem(item);
+    setStateApis(apis.filter((api) => api.progress === item.slice(0, 2)));
+    setDetailApplyId(null);
+    setIsOpened(false);
+  };
+
   const allApis = () => {
     if (apis.length === 0) {
       return <div>신청 api 내역이 없습니다</div>;
@@ -106,69 +230,22 @@ function ApiApproval() {
         <div className="col-span-1 text-center font-medium">
           {api.createDate.split('T')[0]}
         </div>
-        <div
-          role="button"
-          tabIndex={0}
-          className="col-span-3 text-center font-medium"
-          onClick={() => showDetail(api.applyId)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' || e.key === 'Space') {
-              showDetail(api.applyId);
-            }
-          }}
-        >
-          {api.title}
+        <div className="col-span-3 text-center font-medium">
+          <div
+            role="button"
+            tabIndex={0}
+            className="col-span-3 text-center font-medium"
+            onClick={() => showDetail(api.applyId)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' || e.key === 'Space') {
+                showDetail(api.applyId);
+              }
+            }}
+          >
+            {api.title}
+          </div>
         </div>
-        {Object.keys(apiDetail).length === 0 ? (
-          ''
-        ) : (
-          <Modal isOpen={isModalOpen} onClose={closeModal}>
-            <div className="flex flex-col justify-center content-center font-semibold">
-              <div>
-                {/* API 이름 부분 */}
-                <div className="flex">
-                  <p>API명 </p>
-                  <p>API 이름 출력할 부분</p>
-                </div>
-                {/* API 상세 설명 부분 */}
-                <div className="flex">
-                  <p>상세 설명</p>
-                  <p>상세 설명 출력할 부분</p>
-                </div>
-                {/* API 주소 부분 */}
-                <div className="flex">
-                  <p>API </p>
-                  <p>API 주소 출력할 부분</p>
-                </div>
-                {/* 태그 부분 */}
-                <div className="flex">
-                  <p>태그</p>
-                  <p>태그 출력할 부분</p>
-                </div>
-                {/* 인풋 부분 */}
-                <div className="flex">
-                  <p>INPUT</p>
-                  <p>인풋 출력할 부분</p>
-                </div>
-                {/* 아웃풋 부분 */}
-                <div className="flex">
-                  <p>OUTPUT</p>
-                  <p>아웃풋 출력할 부분</p>
-                </div>
-                {/* 제공 신청 날짜 */}
-                <div className="flex">
-                  <p>신청 날짜</p>
-                  <p>신청 날짜 출력할 부분</p>
-                </div>
-                {/* 제공 마감 날짜 */}
-                <div className="flex">
-                  <p>제공 마감 날짜</p>
-                  <p>제공 날짜 출력할 부분</p>
-                </div>
-              </div>
-            </div>
-          </Modal>
-        )}
+
         <div className="col-span-1 text-center">
           <button
             type="button"
@@ -220,25 +297,48 @@ function ApiApproval() {
             ''
           )}
         </div>
+        <div className="col-span-5">
+          {detailApplyId === api.applyId && isOpened && (
+            <ApiSupply apiDetail={apiDetail} />
+          )}
+        </div>
       </div>
     ));
   };
-
   const filterdApis = () => {
     if (stateApis.length === 0) {
-      return <div> {selectedItem.slice(0, 2)} api 내역이 없습니다</div>;
+      return <div>신청 api 내역이 없습니다</div>;
     }
+
     return stateApis.map((api) => (
       <div className="w-full grid grid-cols-5 my-5">
         <div className="col-span-1 text-center font-medium">
           {api.createDate.split('T')[0]}
         </div>
-        <div className="col-span-3 text-center font-medium">{api.title}</div>
+        <div className="col-span-3 text-center font-medium">
+          <div
+            role="button"
+            tabIndex={0}
+            className="col-span-3 text-center font-medium"
+            onClick={() => showDetail(api.applyId)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' || e.key === 'Space') {
+                showDetail(api.applyId);
+              }
+            }}
+          >
+            {api.title}
+          </div>
+        </div>
 
         <div className="col-span-1 text-center">
           <button
             type="button"
-            onClick={() => handleClick(api.applyId, api.title)}
+            onClick={() => {
+              if (api.progress !== '승인' && api.progress !== '거절') {
+                handleClick(api.applyId, api.title);
+              }
+            }}
             className={getClassName(api.progress)}
           >
             {api.progress}
@@ -282,9 +382,37 @@ function ApiApproval() {
             ''
           )}
         </div>
+        <div className="col-span-5">
+          {detailApplyId === api.applyId && isOpened && (
+            <ApiSupply apiDetail={apiDetail} />
+          )}
+        </div>
       </div>
     ));
   };
+
+  const data = [
+    {
+      label: '전체 조회',
+      value: '전체 조회',
+    },
+    {
+      label: '대기 조회',
+      value: '대기 조회',
+    },
+    {
+      label: '승인 조회',
+      value: '승인 조회',
+    },
+    {
+      label: '진행 조회',
+      value: '진행 조회',
+    },
+    {
+      label: '거절 조회',
+      value: '거절 조회',
+    },
+  ];
 
   return (
     <div>
@@ -292,51 +420,22 @@ function ApiApproval() {
 
       <div className="flex justify-arouond w-full mt-8">
         <div className="border-right w-1/6 flex flex-col justify-center items-center text-center my-4">
-          <button
-            type="button"
-            className={
-              selectedItem === '전체 조회' ? 'selected' : 'no-selected'
-            }
-            onClick={() => handleItemClick('전체 조회')}
-          >
-            전체 조회
-          </button>
-          <button
-            type="button"
-            className={
-              selectedItem === '승인 조회' ? 'selected' : 'no-selected'
-            }
-            onClick={() => handleItemClick('승인 조회')}
-          >
-            승인 조회
-          </button>
-          <button
-            type="button"
-            className={
-              selectedItem === '대기 조회' ? 'selected' : 'no-selected'
-            }
-            onClick={() => handleItemClick('대기 조회')}
-          >
-            대기 조회
-          </button>
-          <button
-            type="button"
-            className={
-              selectedItem === '진행 조회' ? 'selected' : 'no-selected'
-            }
-            onClick={() => handleItemClick('진행 조회')}
-          >
-            진행 조회
-          </button>
-          <button
-            type="button"
-            className={
-              selectedItem === '거절 조회' ? 'selected' : 'no-selected'
-            }
-            onClick={() => handleItemClick('거절 조회')}
-          >
-            거절 조회
-          </button>
+          <Tabs value={selectedItem} orientation="vertical">
+            <TabsHeader className="w-40">
+              {data.map(({ label, value }) => (
+                <Tab
+                  key={value}
+                  value={value}
+                  className="place-items-start"
+                  onClick={() => handleItemClick(label)}
+                >
+                  <button type="button" className=" flex items-center gap-2">
+                    {label}
+                  </button>
+                </Tab>
+              ))}
+            </TabsHeader>
+          </Tabs>
         </div>
         <div className="w-5/6 px-8">
           <div className="my-4 w-full grid grid-cols-5 border-bottom py-3">

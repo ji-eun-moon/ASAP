0e1cb48f83c8bfc43703189e21417d13a638dc2b
송아람 <0807com@naver.com>
2023-09-22 13:42:14 +0900

    [FE] FEAT : 지갑 생성 및 회원가입 연동

---
+++ Frontend/asap/src/hooks/api/wallet/useGetWallet.ts
@@ -0,0 +1,26 @@
+import axiosInstance from 'utils/axiosInstance';
+import { useEffect, useState } from 'react';
+
+const useGetWallet = () => {
+  const [wallet, setWallet] = useState<string | null>(null);
+  const getWallet = async () => {
+    try {
+      const response = await axiosInstance({
+        method: 'GET',
+        url: '/api/v1/member/getAddress',
+      });
+      console.log(response.data);
+      setWallet(response.data);
+    } catch (error) {
+      console.log('서버 오류:', error);
+    }
+  };
+
+  useEffect(() => {
+    getWallet();
+  }, []);
+
+  return { getWallet, wallet };
+};
+
+export default useGetWallet;
--- Frontend/asap/src/pages/mypage/AccountCheck.tsx
+++ Frontend/asap/src/pages/mypage/AccountCheck.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Header from 'components/common/Header';
+import useWeb3 from 'hooks/api/wallet/useWeb3';
+import { Button } from '@material-tailwind/react';
 
-function AccountCheck() {
-  return <div>AccountCheck</div>;
+function AccountWallet() {
+  const { createAccount } = useWeb3();
+
+  const onClickHandler = () => {
+    createAccount('a');
+  };
+  return (
+    <div>
+      <Header title="지갑 관리" />
+      <div className="container mx-auto page-container grid grid-cols-4">
+        <div className="col-span-3">
+          <Button onClick={onClickHandler}>지갑 확인</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AccountCheck;
+export default AccountWallet;

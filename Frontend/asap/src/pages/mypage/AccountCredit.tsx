9a40854c38383392585aaad94a8f152cf5dafec4
sion <sion941004@naver.com>
2023-09-22 13:45:02 +0900

    [FE] FEAT : 카드 정보 등록 구현

--- Frontend/asap/src/pages/mypage/AccountCredit.tsx
+++ Frontend/asap/src/pages/mypage/AccountCredit.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import SideBar from 'components/nav/SideBar';
 import Header from 'components/common/Header';
 import menus from 'router/data/mypage-menus';
-import './AccountCredit.scss';
-import { ReactComponent as Cross } from 'assets/icons/Cross2.svg';
+import CreditCardRegistration from 'components/mypage/CreditCardRegistration';
 
 function AccountPay() {
   return (
@@ -19,33 +18,7 @@ function AccountPay() {
           {/* <p className="text-2xl font-bold">등록된 카드</p> */}
           {/* <p className="text-2xl font-bold">결제 내역</p> */}
           {/* 모달창 */}
-          <div className="px-10 py-8 credit-modal border rounded">
-            <div className="flex justify-between cross items-center">
-              <p className="text-2xl font-bold">카드정보</p>
-              <div className="mt-1">
-                <Cross />
-              </div>
-            </div>
-            <div className="mt-2 flex border rounded py-4 px-4">
-              <p>카드번호</p>
-              <p>네모네모</p>
-            </div>
-            <div className="flex">
-              <p>유효 기간</p>
-              <p>유효 기간</p>
-            </div>
-            <div className="flex">
-              <p>카드 비밀번호</p>
-              <p>카드 비밀번호</p>
-            </div>
-            <div className="flex">
-              <p>주민번호</p>
-              <p>주민번호</p>
-            </div>
-            <div>법인 어쩌고</div>
-            <div>법인 어쩌고</div>
-            <div>법인 어쩌고</div>
-          </div>
+          <CreditCardRegistration />
         </div>
       </div>
     </div>

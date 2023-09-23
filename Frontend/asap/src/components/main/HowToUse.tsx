73b36b269566dc0e41d2b8523106a35d9e0c74cb
송아람 <0807com@naver.com>
2023-09-23 16:47:17 +0900

    [FE] FEAT : mainpage 필요 컴포넌트

---
+++ Frontend/asap/src/components/main/HowToUse.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'styles/main/HowToUse.scss';
+
+function HowToUse() {
+  return (
+    <div className="all-container">
+      <div>ASAP에서 당신의 비즈니스를 시작해보세요</div>
+    </div>
+  );
+}
+
+export default HowToUse;
--- Frontend/asap/src/router/data/mypage-menus.ts
+++ Frontend/asap/src/router/data/mypage-menus.ts
@@ -6,9 +6,9 @@ interface IMenu {
 const menus: IMenu[] = [
   { name: '기본 정보', path: '/mypage/account' },
   { name: '키 관리', path: '/mypage/keys' },
+  { name: '지갑 정보', path: '/mypage/wallet' },
   { name: '결제 수단 관리', path: '/mypage/credit' },
   { name: '결제 내역', path: '/mypage/payment' },
-  { name: '지갑 관리', path: '/mypage/wallet' },
 ];
 
 export default menus;

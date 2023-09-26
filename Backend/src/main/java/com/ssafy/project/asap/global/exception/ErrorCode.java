da1669e853d8fd276e76d9058f1a7c313866079d
이도하 <victoryddh5@naver.com>
2023-09-26 17:13:23 +0900

    [BE] FIX : ErrorCode 추가

--- Backend/src/main/java/com/ssafy/project/asap/global/exception/ErrorCode.java
+++ Backend/src/main/java/com/ssafy/project/asap/global/exception/ErrorCode.java
@@ -15,7 +15,7 @@ public enum ErrorCode{
     USER_NOT_HAVE_WALLET(HttpStatus.NOT_FOUND, "지갑이 등록되지 않은 회원입니다."),
     PASSWORD_NOT_AUTHORIZED(HttpStatus.NON_AUTHORITATIVE_INFORMATION, "올바르지 않은 패스워드입니다."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 회원이 존재하지 않습니다."),
-    SIGNUP_DUPLICATED(HttpStatus.CONFLICT, "회원가잆 오류입니다."),
+    SIGNUP_DUPLICATED(HttpStatus.CONFLICT, "회원가입 오류입니다."),
     EMAIL_CODE_NOT_AUTHORIZED(HttpStatus.NON_AUTHORITATIVE_INFORMATION, "올바른 인증번호가 아닙니다."),
     EMAIL_NOT_SEND(HttpStatus.BAD_REQUEST, "인증코드 전송에 실패했습니다."),
     PURPOSE_DUPLICATED(HttpStatus.CONFLICT, "이미 등록된 API입니다."),

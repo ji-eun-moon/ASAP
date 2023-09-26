4df43657cceb2f537b6069c17dd8cf8e724288a5
sion <sion941004@naver.com>
2023-09-26 18:01:00 +0900

    [FE] FEAT : 차트 프레임 구현중

--- Frontend/asap/src/components/chart/chartFrame.tsx
+++ Frontend/asap/src/components/chart/chartFrame.tsx
@@ -8,25 +8,20 @@ import React from 'react';
  */
 
 interface ChartFrameProps {
-  width: string; // 너비를 선택적으로 지정할 수 있도록 변경
-  height: string; // 높이를 선택적으로 지정할 수 있도록 변경
+  width: string;
+  height: string;
   title: string;
 }
 
-function ChartFrame({
-  width = '50px',
-  height = '50px',
-  title,
-}: ChartFrameProps) {
+function ChartFrame({ width, height, title }: ChartFrameProps) {
   const frameStyle = {
     width,
     height,
   };
 
   return (
-    <div className="chart-frame" style={frameStyle}>
-      {/* 왼쪽부분 */}
-      <div className="chart-title">{title}</div>
+    <div className="border" style={frameStyle}>
+      <div className="">{title}</div>
     </div>
   );
 }
--- Frontend/asap/src/pages/test.tsx
+++ Frontend/asap/src/pages/test.tsx
@@ -1,7 +1,14 @@
+import ChartFrame from 'components/chart/chartFrame';
 import React from 'react';
 
 const test = () => {
-  return <div>테스트페이지입니다</div>;
+  return (
+    <div className="ml-32 mt-32">
+      <div>
+        <ChartFrame width="50px" height="50px" title="안녕하세요" />
+      </div>
+    </div>
+  );
 };
 
 export default test;

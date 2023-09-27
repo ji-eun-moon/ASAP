2100010ba3eafec7e65a186a840306f9a605adb7
sion <sion941004@naver.com>
2023-09-27 12:06:32 +0900

    [FE] FEAT : 파이차트 구현

--- Frontend/asap/src/pages/test.tsx
+++ Frontend/asap/src/pages/test.tsx
@@ -1,10 +1,11 @@
 import ChartFrame from 'components/chart/ChartFramee';
 import LineChart from 'components/chart/LineChart';
+import PieChart from 'components/chart/PieChart2';
 import React from 'react';
 
 const test = () => {
   return (
-    <div className="ml-32 mt-32">
+    <div className="ml-32 mt-32 flex">
       <div>
         <ChartFrame
           width="500px"
@@ -12,6 +13,15 @@ const test = () => {
           title="안녕하세요"
           fontSize="20px"
           chart={<LineChart />}
+        />
+      </div>
+      <div className=" ml-10">
+        <ChartFrame
+          width="500px"
+          height="500px"
+          title="안녕하세요"
+          fontSize="20px"
+          chart={<PieChart />}
         />
       </div>
     </div>

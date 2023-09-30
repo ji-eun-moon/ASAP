318316e8118b662be0275a655318ea8315247bc6
victoryddh <victoryddh5@naver.com>
2023-09-30 14:12:37 +0900

    [BE] FIX : @JsonProperty 추가

--- Backend/src/main/java/com/ssafy/project/asap/api/entity/dto/response/ApproveApisResponse.java
+++ Backend/src/main/java/com/ssafy/project/asap/api/entity/dto/response/ApproveApisResponse.java
@@ -1,10 +1,12 @@
 package com.ssafy.project.asap.api.entity.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 @Builder
 public class ApproveApisResponse {
 
+    @JsonProperty("title")
     private String title;
 
 }
--- Backend/src/main/java/com/ssafy/project/asap/purpose/entity/dto/response/FindPurposesResponse.java
+++ Backend/src/main/java/com/ssafy/project/asap/purpose/entity/dto/response/FindPurposesResponse.java
@@ -1,10 +1,12 @@
 package com.ssafy.project.asap.purpose.entity.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 @Builder
 public class FindPurposesResponse {
 
+    @JsonProperty("title")
     private String title;
 
 }

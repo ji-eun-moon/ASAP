886fd5cc0695dd1a0f0f2209e9529cc94c9c52e7
박서희 <dudntjgml@gmail.com>
2023-10-02 16:38:27 +0900

    [FE] FEAT : 동일 카테고리 비교 값 받아오기 및 차트 생성

--- Frontend/asap/src/hooks/api/chart/useCategoryAvg.ts
+++ Frontend/asap/src/hooks/api/chart/useCategoryAvg.ts
@@ -2,20 +2,76 @@
 // 카테고리 평균 조회
 
 import axiosInstance from 'utils/axiosInstance';
+import { useState, useEffect } from 'react';
+import useDetailStore from 'store/chart/useDetailStore';
+import useCategoryStore from 'store/chart/useCategoryStore';
+
+interface categoryAvgInfo {
+  apiId: number;
+  year: string;
+  month: string;
+}
 
 const useCategoryAvg = () => {
-  const categoryAvg = async () => {
+  const [categoryAvgLoading, setCategoryAvgLoading] = useState<boolean>(true);
+
+  const { apiId } = useDetailStore();
+  const { year, month } = useCategoryStore();
+
+  const categoryAvg = async (paramsObject: categoryAvgInfo) => {
     try {
       const response = await axiosInstance({
         method: 'GET',
-        url: '/api/v1/apis/usage/monthly',
+        url: '/api/v1/apis/average/category',
+        params: paramsObject,
       });
-      console.log('사용자 월별 사용량 조회 성공', response.data);
+      setCategoryAvgLoading(false);
+
+      console.log('다른 카테고리 평균 조회 성공', response.data);
     } catch (error) {
-      console.log('사용자 월별 사용량 조회 실패', error);
+      console.log('다른 카테고리 평균 조회 실패', error);
     }
   };
-  return { categoryAvg };
+  useEffect(() => {
+    categoryAvg({ apiId, year, month });
+  }, [apiId, year, month]);
+  return { categoryAvgLoading, categoryAvg };
 };
 
 export default useCategoryAvg;
+
+// //신규 사용자 조회
+// import axiosInstance from 'utils/axiosInstance';
+// import { useState, useEffect, useCallback } from 'react';
+// import useDetailStore from 'store/chart/useDetailStore';
+// // import useCategoryStore from 'store/chart/useCategoryStore';
+
+// // interface categoryAvgInfo {
+// //   apiId: number;
+// // }
+
+// const useCategoryAvg = () => {
+//   const [categoryAvgLoading, setCategoryAvgLoading] = useState<boolean>(true);
+
+//   const { apiId } = useDetailStore();
+
+//   const categoryAvg = useCallback(async () => {
+//     try {
+//       const response = await axiosInstance({
+//         method: 'GET',
+//         url: `/api/v1/purpose/new/${apiId}`,
+//       });
+//       setCategoryAvgLoading(false);
+
+//       console.log('신규 사용자 조회 성공', response.data);
+//     } catch (error) {
+//       console.log('신규 사용자 조회 실패', error);
+//     }
+//   }, [apiId]);
+//   useEffect(() => {
+//     categoryAvg();
+//   }, [categoryAvg]);
+//   return { categoryAvgLoading, categoryAvg };
+// };
+
+// export default useCategoryAvg;
--- Frontend/asap/src/pages/myapi/SupplierApi.tsx
+++ Frontend/asap/src/pages/myapi/SupplierApi.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import SupplierMonthly from 'components/myapi/supplier/SupplierMonthly';
 import SupplierDetail from 'components/myapi/supplier/SupplierDetail';
+import useCategoryAvg from 'hooks/api/chart/useCategoryAvg';
 
 function SupplierApi() {
+  const { categoryAvg } = useCategoryAvg();
   return (
     <div>
       {/* 제공자 월별 통계 */}
       <SupplierMonthly />
       {/* 제공자 상세 통계 */}
       <SupplierDetail />
+      <button type="button" onClick={() => categoryAvg}>
+        click
+      </button>
     </div>
   );
 }

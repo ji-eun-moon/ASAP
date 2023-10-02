7067cebcec79ee189ddea03fce3f33480473aed2
문지은 <mjieun0956@gmail.com>
2023-10-02 23:06:40 +0900

    [FE] FEAT : User 30일 사용량 스토어 구현
    Change-Id: Id699238ce68c61970249b30c4b8c1bd0d3b9f210

--- Frontend/asap/src/components/chart/LineGradient.tsx
+++ Frontend/asap/src/components/chart/LineGradient.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useCallback } from 'react';
+import useAuthStore from 'store/auth/useAuthStore';
 import * as echarts from 'echarts';
 
 interface IDailyUsage {
@@ -13,92 +14,126 @@ interface Props {
 
 function LineGradient({ data }: Props) {
   const chartRef = useRef<HTMLDivElement | null>(null);
+  const { loginType } = useAuthStore((state) => state);
 
-  const dateList = data.map((item) => item.date);
-  const amountList = data.map((item) => item.amount);
-  const priceList = data.map((item) => item.price);
-
-  const [options] = useState({
-    visualMap: [
-      {
-        show: false,
-        type: 'continuous',
-        seriesIndex: 0,
-        min: 0,
-        max: 400,
-      },
-      {
-        show: false,
-        type: 'continuous',
-        seriesIndex: 1,
-        dimension: 0,
-        min: 0,
-        max: dateList.length - 1,
-      },
-    ],
-
-    title: [
+  const getTitle = useCallback(() => {
+    if (loginType === 'supplier') {
+      return [
+        {
+          left: 'center',
+          text: '최근 30일 제공량',
+          textStyle: {
+            fontWeight: 'bold',
+            fontSize: 18,
+          },
+        },
+        {
+          top: '50%',
+          left: 'center',
+          text: '최근 30일 수익',
+          textStyle: {
+            fontWeight: 'bold',
+            fontSize: 18,
+          },
+        },
+      ];
+    }
+    return [
       {
         left: 'center',
-        text: 'amount',
+        text: '최근 30일 요청량',
+        textStyle: {
+          fontWeight: 'bold',
+          fontSize: 18,
+        },
       },
       {
-        top: '55%',
+        top: '50%',
         left: 'center',
-        text: 'price',
+        text: '최근 30일 사용 요금',
+        textStyle: {
+          fontWeight: 'bold',
+          fontSize: 18,
+        },
       },
-    ],
-    tooltip: {
-      trigger: 'axis',
-    },
-    xAxis: [
-      {
-        data: dateList,
-      },
-      {
-        data: dateList,
-        gridIndex: 1,
-      },
-    ],
-    yAxis: [
-      {},
-      {
-        gridIndex: 1,
-      },
-    ],
-    grid: [
-      {
-        bottom: '60%',
-      },
-      {
-        top: '60%',
-      },
-    ],
-    series: [
-      {
-        type: 'line',
-        showSymbol: false,
-        data: amountList,
-      },
-      {
-        type: 'line',
-        showSymbol: false,
-        data: priceList,
-        xAxisIndex: 1,
-        yAxisIndex: 1,
-      },
-    ],
-  });
+    ];
+  }, [loginType]);
 
   useEffect(() => {
-    if (chartRef.current) {
-      const chart = echarts.init(chartRef.current);
+    if (!chartRef.current) return undefined;
 
-      chart.setOption(options);
-    }
-  }, [options, chartRef, data]);
+    const dateList = data.map((item) => item.date);
+    const amountList = data.map((item) => item.amount);
+    const priceList = data.map((item) => item.price);
 
-  return <div ref={chartRef} style={{ width: '100%', height: '400px' }} />;
+    const options = {
+      visualMap: [
+        {
+          show: false,
+          type: 'continuous',
+          seriesIndex: 0,
+          min: 0,
+          max: 400,
+        },
+        {
+          show: false,
+          type: 'continuous',
+          seriesIndex: 1,
+          dimension: 0,
+          min: 0,
+          max: dateList.length - 1,
+        },
+      ],
+      title: getTitle(),
+      tooltip: {
+        trigger: 'axis',
+      },
+      xAxis: [
+        {
+          data: dateList,
+        },
+        {
+          data: dateList,
+          gridIndex: 1,
+        },
+      ],
+      yAxis: [
+        {},
+        {
+          gridIndex: 1,
+        },
+      ],
+      grid: [
+        {
+          bottom: '60%',
+        },
+        {
+          top: '60%',
+        },
+      ],
+      series: [
+        {
+          type: 'line',
+          showSymbol: false,
+          data: amountList,
+        },
+        {
+          type: 'line',
+          showSymbol: false,
+          data: priceList,
+          xAxisIndex: 1,
+          yAxisIndex: 1,
+        },
+      ],
+    };
+
+    const chart = echarts.init(chartRef.current);
+    chart.setOption(options);
+
+    return () => chart.dispose();
+  }, [data, getTitle]);
+
+  return <div ref={chartRef} style={{ width: '100%', height: '700px' }} />;
 }
 
 export default LineGradient;
--- Frontend/asap/src/store/chart/useDetailStore.ts
+++ Frontend/asap/src/store/chart/useDetailStore.ts
@@ -9,15 +9,19 @@ interface IDailyUsage {
 interface IDetailStore {
   // axios 요청할 apiId
   apiId: number;
+  apiTitle: string;
   dailyUsageStore: IDailyUsage[] | null;
   setDailyUsageStore: (dailyUsageStore: IDailyUsage[]) => void; // eslint-disable-line no-unused-vars
   setApiId: (apiId: number) => void; // eslint-disable-line no-unused-vars
+  setApiTitle: (apiTitle: string) => void; // eslint-disable-line no-unused-vars
 }
 
 const useDetailStore = create<IDetailStore>((set) => ({
   apiId: 23,
+  apiTitle: '',
   dailyUsageStore: null,
   setApiId: (apiId: number) => set({ apiId }),
+  setApiTitle: (apiTitle: string) => set({ apiTitle }),
   setDailyUsageStore: (dailyUsageStore: IDailyUsage[]) =>
     set({ dailyUsageStore }),
 }));

69267472a53db85e889320ae087e0f2a7a91d437
송아람 <0807com@naver.com>
2023-10-04 21:42:49 +0900

    [FE] FEAT : 검증페이지 기능 및 디자인

--- Frontend/asap/src/pages/blockchain/CheckUsage.tsx
+++ Frontend/asap/src/pages/blockchain/CheckUsage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, ChangeEvent } from 'react';
-import { useNavigate } from 'react-router-dom';
 import useCheckUsage from 'hooks/api/blockchain/useCheckUsage';
 import useWeb3 from 'hooks/api/wallet/useWeb3';
 import useGetUseList from 'hooks/api/chart/useGetUseList';
@@ -7,17 +6,16 @@ import Header from 'components/common/Header';
 import HowToUse from 'components/blockchain/HowToUse';
 import Calendar from 'components/blockchain/Calendar';
 import Dropdown from 'components/blockchain/Dropdown';
-import Modal from 'components/common/Modal';
 import { SHA256 } from 'crypto-js';
 import explainIcon from 'assets/icons/Explain.png';
 import { ReactComponent as Copy } from 'assets/icons/copybutton.svg';
 import { ReactComponent as Cal } from 'assets/icons/Calendar.svg';
+import Swal from 'sweetalert2';
 import 'styles/blockchain/CheckUsage.scss';
-
-// import Spinner from 'components/common/Spinner';
+import Spinner from 'components/common/Spinner';
+import ScrollToTop from 'components/common/ScrollToTop';
 
 function CheckUsage() {
-  const navigate = useNavigate();
   const { checkUsage } = useCheckUsage();
   const { useList } = useGetUseList();
   const { getTransaction } = useWeb3();
@@ -25,28 +23,25 @@ function CheckUsage() {
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
   const [apiTitle, setApiTitle] = useState<string | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
+  const [displayedChars, setDisplayedChars] = useState<number>(1482);
+  const [loading, setLoading] = useState<boolean>(false);
   const [test, setTest] = useState<string>('');
   const [hash, setHash] = useState<string | null>(null);
-  const [database, setDatabase] = useState<string | null>(null);
+  const [database, setDatabase] =
+    useState<string>('아직 조회된 데이터가 없습니다');
   const [recordHash, setRecordHash] = useState<string | null>(null);
-  const [txInput, setTxInput] = useState<string | undefined | null>(null);
+  const [txInput, setTxInput] = useState<string | undefined>(
+    '아직 조회된 데이터가 없습니다',
+  );
   const [header, setHeader] = useState<string | undefined | null>(null);
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
 
   const onHowToUseHandler = () => {
     setIsOpen(!isOpen);
     console.log(isOpen, '확인');
   };
 
-  const onPageHandler = () => {
-    navigate('/myapi');
-  };
   const onInputHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setTest(event.target.value);
   };
@@ -85,11 +80,12 @@ function CheckUsage() {
       setStartDate(null);
       setEndDate(null);
     }
-    setDatabase(null);
-    setTxInput(null);
+    setDatabase('아직 조회된 데이터가 없습니다');
+    setTxInput('아직 조회된 데이터가 없습니다');
     setHeader(null);
     setHash(null);
     setTest('');
+    setRecordHash(null);
   };
 
   // 해시 변환하기
@@ -105,7 +101,9 @@ function CheckUsage() {
 
   // 검증하기 버튼 클릭시 checkUsage 실행
   const onCheckHandler = async () => {
+    setDisplayedChars(1482);
     if (startDate && endDate && apiTitle) {
+      setLoading(true);
       const formattedStartDate = formatDate(startDate);
       const formattedEndDate = formatDate(endDate);
 
@@ -116,6 +114,7 @@ function CheckUsage() {
       });
 
       if (data) {
+        setLoading(false);
         setDatabase(data.hashToString);
         setRecordHash(data.recordHash);
         // transaction hash로 트랜잭션 조회
@@ -125,12 +124,27 @@ function CheckUsage() {
         setHeader(`[${head}]`);
         setTxInput(rest);
 
-        setIsModalOpen(true);
+        if (rest === data.recordHash) {
+          Swal.fire({
+            icon: 'success',
+            title: '검증 결과',
+            text: '데이터가 일치합니다.',
+            confirmButtonColor: '#4caf50',
+          });
+        } else {
+          Swal.fire({
+            icon: 'warning',
+            title: '검증 결과',
+            text: '데이터가 일치하지 않습니다.',
+            confirmButtonColor: '#f44336',
+          });
+        }
       } else {
         setHeader(null);
-        setTxInput('403');
-        setDatabase('403');
+        setTxInput('해당 기간에 사용한 내역이 없습니다');
+        setDatabase('해당 기간에 사용한 내역이 없습니다');
         setRecordHash('403');
+        setLoading(false);
       }
     }
   };
@@ -226,7 +240,19 @@ function CheckUsage() {
                   </button>
                 </div>
               </div>
-              {hash ? <div className="check-back data mt-4">{hash}</div> : null}
+              {hash ? (
+                <div className="check-back flex justify-between data mt-4">
+                  <div>{hash}</div>
+                  <div className="color-text">
+                    {recordHash && recordHash !== '403' && hash === txInput
+                      ? '일치합니다'
+                      : null}
+                    {recordHash && recordHash !== '403' && hash !== txInput
+                      ? '일치하지 않습니다'
+                      : null}
+                  </div>
+                </div>
+              ) : null}
             </div>
           </div>
         </div>
@@ -236,9 +262,10 @@ function CheckUsage() {
           </div>
           <div className="w-9/12">
             <div className="check-back data">
-              {!txInput ? '아직 선택된 데이터가 없습니다' : null}
-              {txInput === '403' ? (
-                '해당 기간에 사용한 내역이 없습니다'
+              {loading ? (
+                <div className="flex w-full justify-center">
+                  <Spinner size="7" />
+                </div>
               ) : (
                 <div>
                   <span>{header}</span>
@@ -246,7 +273,7 @@ function CheckUsage() {
                 </div>
               )}
             </div>
-            {txInput && txInput !== '403' ? (
+            {header ? (
               <div className="mt-4 text-sm text-gray-600 font-medium">
                 ※ 괄호 속 10자리는 스마트 컨트랙트에서 사용되는 함수 시그니처로
                 데이터 검증과 무관합니다.
@@ -259,17 +286,28 @@ function CheckUsage() {
             정산에 사용된 데이터
           </div>
           <div className="check-back data w-9/12">
-            {/* {database && database !== '403' ? (
-              <Copy className="copy" onClick={handleCopyClipBoard} />
-            ) : null} */}
-            {!database ? '아직 선택된 데이터가 없습니다' : null}
-            {database && database === '403' ? (
-              '해당 기간에 사용한 내역이 없습니다'
+            {loading ? (
+              <div className="flex w-full justify-center">
+                <Spinner size="7" />
+              </div>
             ) : (
               <div className="flex">
-                <div>{database}</div>
+                <div className="flex flex-col">
+                  <div className="break-all">
+                    {database?.slice(0, displayedChars)}
+                  </div>
+                  {database && database.length > displayedChars && (
+                    <button
+                      type="button"
+                      onClick={() => setDisplayedChars((prev) => prev + 1482)}
+                      className="load-more-button"
+                    >
+                      더보기
+                    </button>
+                  )}
+                </div>
                 <div>
-                  {database ? (
+                  {database && recordHash && recordHash !== '403' ? (
                     <Copy className="copy" onClick={handleCopyClipBoard} />
                   ) : null}
                 </div>
@@ -278,35 +316,7 @@ function CheckUsage() {
           </div>
         </div>
       </div>
-
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <div className="flex flex-col">
-          <div className="flex text-xl font-bold justify-center mb-5">
-            검증결과
-          </div>
-          {/* <div>블록체인에 저장된 데이터의 해시값 {txInput}</div>
-          <div>정산에 사용된 데이터의 해시값 {recordHash}</div> */}
-          <div className="flex justify-center">
-            {txInput === recordHash ? '동일합니다' : '동일하지 않습니다'}
-          </div>
-          <div className="flex justify-center">
-            <button
-              type="button"
-              onClick={closeModal}
-              className="modal-button back-sky"
-            >
-              자세히 보기
-            </button>
-            <button
-              type="button"
-              onClick={onPageHandler}
-              className="modal-button back-blue"
-            >
-              통계보기
-            </button>
-          </div>
-        </div>
-      </Modal>
+      <ScrollToTop />
     </div>
   );
 }

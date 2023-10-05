1229f7e68655d45e4f31438bbe681ee403c8a18c
송아람 <0807com@naver.com>
2023-10-05 12:05:24 +0900

    [FE] FEAT : 검증페이지 피드백 수정

--- Frontend/asap/src/pages/blockchain/CheckUsage.tsx
+++ Frontend/asap/src/pages/blockchain/CheckUsage.tsx
@@ -6,6 +6,7 @@ import Header from 'components/common/Header';
 import HowToUse from 'components/blockchain/HowToUse';
 import Calendar from 'components/blockchain/Calendar';
 import Dropdown from 'components/blockchain/Dropdown';
+import TooltipHelper from 'components/common/TooltipHelper';
 import { SHA256 } from 'crypto-js';
 import explainIcon from 'assets/icons/Explain.png';
 import { ReactComponent as Copy } from 'assets/icons/copybutton.svg';
@@ -176,8 +177,9 @@ function CheckUsage() {
           />
         </div>
         <div className="flex items-center my-8">
-          <div className="text-xl font-bold pr-7 w-3/12">
+          <div className="text-xl font-bold pr-7 w-3/12 flex">
             조회할 API를 선택하세요
+            <TooltipHelper message="사용중인 API 중 검증이 필요한 API를 선택하세요" />
           </div>
           {useList ? (
             <Dropdown
@@ -189,8 +191,18 @@ function CheckUsage() {
           ) : null}
         </div>
         <div className="flex items-center my-8">
-          <div className="text-xl font-bold pr-7 w-3/12">
+          <div className="text-xl font-bold pr-7 w-3/12 flex">
             조회 기간을 선택하세요
+            <TooltipHelper
+              message={
+                <div>
+                  <div>
+                    ASAP에서는 일주일 간격으로 API 사용량을 블록에 저장합니다
+                  </div>
+                  <div>검증을 원하는 주를 선택하세요</div>
+                </div>
+              }
+            />
           </div>
           <label className="flex items-center pick-date">
             <Cal className="h-5" />
@@ -223,8 +235,16 @@ function CheckUsage() {
         </div>
         <hr />
         <div className="flex items-start my-4">
-          <div className="text-xl font-bold w-3/12">
+          <div className="text-xl font-bold w-3/12 flex">
             데이터를 해시값으로 변환하기
+            <TooltipHelper
+              message={
+                <div>
+                  <div>원하는 데이터를 입력 후 버튼을 클릭하면</div>
+                  <div>해시값으로 직접 변환 및 검증이 가능합니다</div>
+                </div>
+              }
+            />
           </div>
           <div className="flex w-9/12">
             <div className="flex flex-col w-full">
@@ -247,7 +267,7 @@ function CheckUsage() {
                     onClick={onHashHandler}
                     className="w-full check-button"
                   >
-                    변환하기
+                    변환 및 검증
                   </button>
                 </div>
               </div>
@@ -268,14 +288,18 @@ function CheckUsage() {
           </div>
         </div>
         <div className="flex items-start my-4">
-          <div className="text-xl font-bold w-3/12 mt-2">
+          <div className="text-xl font-bold w-3/12 mt-2 flex">
             블록에 저장된 데이터
+            <TooltipHelper
+              message="블록에 저장된 데이터와 정산에 사용된 데이터의 해시값이 동일하다면 데이터가
+              변조되지 않았음을 의미합니다"
+            />
           </div>
           <div className="w-9/12">
             <div className="check-back data">
               {loading ? (
                 <div className="flex w-full justify-center">
-                  <Spinner size="7" />
+                  <Spinner size="6.5" />
                 </div>
               ) : (
                 <div>
@@ -293,13 +317,23 @@ function CheckUsage() {
           </div>
         </div>
         <div className="flex items-start my-4">
-          <div className="text-xl font-bold w-3/12 mt-2">
+          <div className="text-xl font-bold w-3/12 mt-2 flex">
             정산에 사용된 데이터
+            <TooltipHelper
+              message={
+                <div>
+                  <div>
+                    오른쪽 상단의 복사버튼 클릭한 뒤 데이터 변환칸에 입력하면
+                  </div>
+                  <div>해시값 변환 및 직접 검증이 가능합니다</div>
+                </div>
+              }
+            />
           </div>
           <div className="check-back data w-9/12">
             {loading ? (
               <div className="flex w-full justify-center">
-                <Spinner size="7" />
+                <Spinner size="6.5" />
               </div>
             ) : (
               <div className="flex">
@@ -313,7 +347,7 @@ function CheckUsage() {
                       onClick={() => setDisplayedChars((prev) => prev + 1482)}
                       className="load-more-button"
                     >
-                      더보기
+                      이후 데이터를 보고싶다면 클릭하세요
                     </button>
                   )}
                 </div>
